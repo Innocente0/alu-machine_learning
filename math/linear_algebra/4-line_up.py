e43e365cc4721e7f852c73e3062ef4d55a1e819c
Innocente0 <i.niwemwana@alustudent.com>
2025-05-24 11:49:31 -0700

    Line Up

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
+"""
+module: 4-line_up
+
+Provides add_arrays(arr1, arr2) which returns a new list containing the
+element-wise sums of two equally sized lists, or None if their lengths differ.
+"""
+
+
 def add_arrays(arr1, arr2):
     """
-    Add two 1-D arrays (lists) element-wise and return a new list.
-    If the lists have different lengths, return None.
+    Add two 1-D arrays (lists) element-wise.
+    Return a new list of sums, or None if lengths don’t match.
     """
-    # 1) Check that they’re the same length
     if len(arr1) != len(arr2):
         return None
 
-    # 2) Build a new list by summing corresponding elements
     result = []
     for i in range(len(arr1)):
         result.append(arr1[i] + arr2[i])
