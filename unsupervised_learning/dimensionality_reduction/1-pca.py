d7f1d15cb2e0f13fac78d1874f0164dacae369fe
Innocente Niwemwana <i.niwemwana@alustudent.com>
2026-03-16 15:24:28 +0200

    pca file

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -8,11 +8,11 @@ def pca(X, ndim):
     """performs PCA on a dataset"""
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None
-    if not isinstance(ndim, int) or ndim <= 0 or ndim > X.shape[1]:
+    if not isinstance(ndim, int) or ndim <= 0 or ndim > min(X.shape):
         return None
 
     X_centered = X - np.mean(X, axis=0)
-    U, S, Vt = np.linalg.svd(X_centered, full_matrices=False)
-    T = np.matmul(U[:, :ndim], np.diag(S[:ndim]))
+    U, S, _ = np.linalg.svd(X_centered, full_matrices=False)
+    T = U[:, :ndim] * S[:ndim]
 
     return T
