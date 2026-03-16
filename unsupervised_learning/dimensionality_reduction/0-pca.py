bc59be6f9a448203fdacce0711d060f5cc4d57e9
Innocente Niwemwana <i.niwemwana@alustudent.com>
2026-03-16 14:57:43 +0200

    pca file

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""PCA module"""
+
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """performs PCA on a dataset"""
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None
+
+    u, s, vh = np.linalg.svd(X, full_matrices=False)
+    explained_variance = np.cumsum(s ** 2) / np.sum(s ** 2)
+    nd = np.searchsorted(explained_variance, var) + 1
+
+    W = vh.T[:, :nd]
+    return W
